2babbce5ee5210955f04d2d9f0522bce095e30dc
vivekramamoorthy <vivek.ramamoorthy@hpe.com>
2016-10-05 12:01:57 -0700

    Update generator.py

--- lib/topology_lib_scapy/generator.py
+++ lib/topology_lib_scapy/generator.py
@@ -106,7 +106,7 @@ def createcdmline(packet, packet_struct, packet_list, options):
             packet = packet + "Dot1Q("
             packet = get_pkt_optns(r'\s*802.1Q\w*', packet, packet_list)
         # Match ICMPv6_Req packet
-        elif re.match(r'\s*icmpv6_req', pkt_type, re.IGNORECASE):
+        elif re.match(r'\s*ICMPv6_REQ', pkt_type, re.IGNORECASE):
             packet = packet + "ICMPv6EchoRequest("
             packet = get_pkt_optns(r'\s*icmp\w*', packet, packet_list)            
 
